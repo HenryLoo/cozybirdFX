857f166dfbf4270979fe7d186bcd2b674f35c657
Henry Loo <henry_loo@live.ca>
2020-03-23 20:11:21 -0700

    Add smoke preset

--- cozybirdFX/NewState.cpp
+++ cozybirdFX/NewState.cpp
@@ -24,6 +24,13 @@ namespace
 		"fireball4",
 		"fireball5"
 	};
+
+	const std::vector<std::string> SMOKE_TEXTURES{
+		"smoke1",
+		"smoke2",
+		"smoke3",
+		"cloud3",
+	};
 }
 
 NewState::NewState(Engine &engine, AssetLoader &assetLoader, 
@@ -124,7 +131,58 @@ void NewState::initMenu()
 		{
 			eRenderer.init(assetLoader);
 
-			// TODO;
+			eRenderer.setClipSize(glm::ivec2(250, 320));
+
+			Emitter &emitter{ eRenderer.getEmitter(0) };
+			emitter.setPosition(glm::vec2(0.f, -90.f));
+			emitter.setNumToGenerate(glm::linearRand(4, 7));
+			emitter.setTimeToSpawn(getRandomFloat(0.6f, 0.9f));
+
+			emitter.setSpeedMin(0.f);
+			emitter.setSpeedMax(getRandomFloat(60.f, 90.f));
+			emitter.setSpeedGrowth(0.f);
+
+			int dirMin{ glm::linearRand(60, 80) };
+			emitter.setDirectionMin(dirMin);
+			const int DIR_CENTER{ 90 };
+			emitter.setDirectionMax(DIR_CENTER * 2 - dirMin);
+			emitter.setDirectionGrowth(0);
+
+			emitter.setRotationMin(0);
+			emitter.setRotationMax(359);
+			emitter.setRotationGrowth(glm::linearRand(5, 30));
+
+			float sizeMin{ getRandomFloat(70.f, 90.f) };
+			emitter.setSizeMin(sizeMin);
+			emitter.setSizeMax(getRandomFloat(sizeMin, sizeMin + 20.f));
+
+			float lifeMin{ getRandomFloat(2.5f, 2.8f) };
+			emitter.setLifeMin(lifeMin);
+			float lifeMax{ getRandomFloat(lifeMin, lifeMin + 0.2f) };
+			emitter.setLifeMax(lifeMax);
+
+			emitter.setBlendMode(Emitter::BlendMode::Linear);
+
+			float col{ getRandomFloat(0.45f, 0.55f, 3) };
+			float colOffset{ 0.1f };
+			emitter.setColour(glm::vec4(
+				getRandomFloat(col - colOffset, col + colOffset, 3),
+				getRandomFloat(col - colOffset, col + colOffset, 3),
+				getRandomFloat(col - colOffset, col + colOffset, 3),
+				getRandomFloat(0.7f, 1.f, 3)));
+			emitter.setBirthColour(glm::vec4(
+				getRandomFloat(col - colOffset, col + colOffset, 3),
+				getRandomFloat(col - colOffset, col + colOffset, 3),
+				getRandomFloat(col - colOffset, col + colOffset, 3),
+				0.f));
+			emitter.setDeathColour(glm::vec4(
+				getRandomFloat(col - colOffset / 2, col + colOffset / 2, 3),
+				getRandomFloat(col - colOffset / 2, col + colOffset / 2, 3),
+				getRandomFloat(col - colOffset / 2, col + colOffset / 2, 3),
+				0.f));
+
+			int textureIndex{ glm::linearRand(0, static_cast<int>(SMOKE_TEXTURES.size()) - 1) };
+			emitter.setTexture(assetLoader, SMOKE_TEXTURES[textureIndex] + ".png");
 
 			engine.popState();
 			engine.popState();
